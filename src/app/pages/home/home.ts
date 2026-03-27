a8e14433e44b6f9683cbecfad63654422d22f7a1
marcos <marcoslopezmarinn@gmail.com>
2026-03-27 22:48:35 +0100

    refactor: rename `HeroComponent` to `HomeComponent` and `HomeComponent` page to `HomeComponentPage` for naming clarity.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { HeroComponent } from '../../components/sections/home/home';
+import { HomeComponent } from '../../components/sections/home/home';
 import { AboutComponent } from '../../components/sections/about/about';
 import { SkillsComponent } from '../../components/sections/skills/skills';
 import { ProjectsComponent } from '../../components/sections/projects/projects';
 import { ContactComponent } from '../../components/sections/contact/contact';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-home-page',
   standalone: true,
-  imports: [HeroComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent],
+  imports: [HomeComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent],
   templateUrl: './home.html',
 })
-export class HomeComponent {}
+export class HomeComponentPage {}
