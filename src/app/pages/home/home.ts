b76f99673d5e07bf83d75a400cc157426b051663
marcos <marcoslopezmarinn@gmail.com>
2026-03-28 15:48:49 +0100

    Other-secction-V2

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from '../../components/sections/home/home';
+import { HomeComponent as HomeSectionComponent } from '../../components/sections/home/home';
 import { AboutComponent } from '../../components/sections/about/about';
 import { SkillsComponent } from '../../components/sections/skills/skills';
 import { ProjectsComponent } from '../../components/sections/projects/projects';
@@ -8,7 +8,13 @@ import { ContactComponent } from '../../components/sections/contact/contact';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HomeComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent],
+  imports: [
+    HomeSectionComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent,
+  ],
   templateUrl: './home.html',
 })
-export class HomeComponentPage {}
+export class HomeComponent {}
